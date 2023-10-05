8963cbf2460bc4ec8e89b395d9c03ef4f6997111
Quentin Santos <qsantos@qsantos.fr>
2023-10-05 16:07:30 +0200

    Make remove() correct

--- src/htree.rs
+++ src/htree.rs
@@ -3,11 +3,12 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 
 struct Node<K, V> {
-    hash: u64,
     key: K,
     value: V,
+    self_hash: u64,
     left: Option<Box<Node<K, V>>>,
     right: Option<Box<Node<K, V>>>,
+    tree_hash: u64,
 }
 
 fn hash<K: Hash, V: Hash>(key: &K, value: &V) -> u64 {
@@ -19,21 +20,25 @@ fn hash<K: Hash, V: Hash>(key: &K, value: &V) -> u64 {
 
 impl<K: Hash, V: Hash> Node<K, V> {
     fn new(key: K, value: V) -> Self {
+        let hash = hash(&key, &value);
         Node {
-            hash: hash(&key, &value),
             key,
             value,
+            self_hash: hash,
             left: None,
             right: None,
+            tree_hash: hash,
         }
     }
 
     fn replace(&mut self, value: V) -> (u64, V) {
-        let old_hash = self.hash;
+        let old_hash = self.self_hash;
         let new_hash = hash(&self.key, &value);
-        self.hash = new_hash;
+        self.self_hash = new_hash;
         let old_value = std::mem::replace(&mut self.value, value);
-        (new_hash ^ old_hash, old_value)
+        let diff_hash = new_hash ^ old_hash;
+        self.tree_hash ^= diff_hash;
+        (diff_hash, old_value)
     }
 }
 
@@ -54,7 +59,7 @@ impl<K: Hash + Ord, V: Hash> HTree<K, V> {
 
     pub fn hash(&self) -> u64 {
         match &self.root {
-            Some(node) => node.hash,
+            Some(node) => node.tree_hash,
             None => 0,
         }
     }
@@ -73,18 +78,18 @@ impl<K: Hash + Ord, V: Hash> HTree<K, V> {
                     }
                     Ordering::Less => {
                         let (diff_hash, old_node) = aux(&mut node.left, key, value);
-                        node.hash ^= diff_hash;
+                        node.tree_hash ^= diff_hash;
                         (diff_hash, old_node)
                     }
                     Ordering::Greater => {
                         let (diff_hash, old_node) = aux(&mut node.right, key, value);
-                        node.hash ^= diff_hash;
+                        node.tree_hash ^= diff_hash;
                         (diff_hash, old_node)
                     }
                 }
             } else {
                 let node = Box::new(Node::new(key, value));
-                let hash = node.hash;
+                let hash = node.self_hash;
                 *anchor = Some(node);
                 (hash, None)
             }
@@ -93,13 +98,21 @@ impl<K: Hash + Ord, V: Hash> HTree<K, V> {
     }
 
     pub fn remove(&mut self, key: &K) -> Option<V> {
-        fn leftmost<K, V>(mut node: &mut Box<Node<K, V>>) -> Box<Node<K, V>> {
-            while node.left.as_mut().unwrap().left.is_some() {
-                node = node.left.as_mut().unwrap();
+        fn take_leftmost<K, V>(node: &mut Box<Node<K, V>>) -> Box<Node<K, V>> {
+            if node.left.as_mut().unwrap().left.is_some() {
+                let ret = take_leftmost(node.left.as_mut().unwrap());
+                node.tree_hash ^= ret.self_hash;
+                ret
+            } else {
+                let mut leftmost = node.left.take().unwrap();
+                node.tree_hash ^= leftmost.self_hash;
+                if let Some(right) = leftmost.right.take() {
+                    leftmost.tree_hash ^= right.tree_hash;
+                    node.left = Some(right);
+                }
+                assert_eq!(leftmost.self_hash, leftmost.tree_hash);
+                leftmost
             }
-            let mut ret = node.left.take().unwrap();
-            node.left = ret.right.take();
-            ret
         }
         fn aux<K: Hash + Ord, V>(
             anchor: &mut Option<Box<Node<K, V>>>,
@@ -109,18 +122,20 @@ impl<K: Hash + Ord, V: Hash> HTree<K, V> {
                 match key.cmp(&node.key) {
                     Ordering::Equal => {
                         let mut node = anchor.take().unwrap();
-                        let ret = (node.hash, Some(node.value));
+                        let ret = (node.self_hash, Some(node.value));
                         match (node.left.take(), node.right.take()) {
                             (None, None) => (),
                             (None, Some(right)) => *anchor = Some(right),
                             (Some(left), None) => *anchor = Some(left),
                             (Some(left), Some(mut right)) => {
                                 if right.left.is_some() {
-                                    let mut next_node = leftmost(&mut right);
+                                    let mut next_node = take_leftmost(&mut right);
+                                    next_node.tree_hash ^= left.tree_hash ^ right.tree_hash;
                                     next_node.left = Some(left);
                                     next_node.right = Some(right);
                                     *anchor = Some(next_node);
                                 } else {
+                                    right.tree_hash ^= left.tree_hash;
                                     right.left = Some(left);
                                     *anchor = Some(right);
                                 }
@@ -130,12 +145,12 @@ impl<K: Hash + Ord, V: Hash> HTree<K, V> {
                     }
                     Ordering::Less => {
                         let (diff_hash, old_node) = aux(&mut node.left, key);
-                        node.hash ^= diff_hash;
+                        node.tree_hash ^= diff_hash;
                         (diff_hash, old_node)
                     }
                     Ordering::Greater => {
                         let (diff_hash, old_node) = aux(&mut node.right, key);
-                        node.hash ^= diff_hash;
+                        node.tree_hash ^= diff_hash;
                         (diff_hash, old_node)
                     }
                 }
@@ -163,14 +178,17 @@ impl<K: Hash + Ord, V: Hash> HTree<K, V> {
                         panic!("Ordering invariant violated");
                     }
                 }
+                let self_hash = hash(&node.key, &node.value);
+                if self_hash != node.self_hash {
+                    panic!("Self hashing invariant violated");
+                }
                 let left_hash = aux(&node.left, min, Some(&node.key));
                 let right_hash = aux(&node.right, Some(&node.key), max);
-                let local_hash = hash(&node.key, &node.value);
-                let expected_hash = left_hash ^ right_hash ^ local_hash;
-                if expected_hash != node.hash {
-                    panic!("Hashing invariant violated");
+                let tree_hash = left_hash ^ right_hash ^ self_hash;
+                if tree_hash != node.tree_hash {
+                    panic!("Tree hashing invariant violated");
                 }
-                expected_hash
+                tree_hash
             } else {
                 0
             }
@@ -245,22 +263,28 @@ mod tests {
         let mut tree = super::HTree::new();
         let mut key_values = Vec::new();
 
+        let mut expected_hash = 0;
+
         // add some
         for _ in 0..1000 {
             let key: u64 = rng.gen();
             let value: u64 = rng.gen();
             tree.insert(key, value);
             tree.validate();
+            expected_hash ^= super::hash(&key, &value);
+            assert_eq!(tree.hash(), expected_hash);
             key_values.push((key, value));
         }
 
         // remove some
         key_values.shuffle(&mut rng);
-        for _ in 0..100 {
+        for _ in 0..1000 {
             let (key, value) = key_values.pop().unwrap();
             let value2 = tree.remove(&key);
             tree.validate();
             assert_eq!(value2, Some(value));
+            expected_hash ^= super::hash(&key, &value);
+            assert_eq!(tree.hash(), expected_hash);
         }
     }
 }
