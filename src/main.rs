3282ea7bd7877802993b3cc10af75e29e02a29c9
Quentin Santos <qsantos@qsantos.fr>
2023-10-17 21:12:25 +0200

    Merge send_queries into answer_queries

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ use std::fmt::Debug;
 use std::hash::Hash;
 use std::net::SocketAddr;
 use std::sync::{Arc, RwLock};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use bincode::{DefaultOptions, Deserializer, Serializer};
 use clap::Parser;
-use futures::future::{select, Either};
 use rand::{
     distributions::{Alphanumeric, DistString},
     SeedableRng,
@@ -31,127 +30,126 @@ async fn answer_queries<
     V: Clone + DeserializeOwned + Hash + Serialize,
 >(
     socket: Arc<UdpSocket>,
+    other_addr: SocketAddr,
     tree: Arc<RwLock<HTree<K, V>>>,
 ) -> Result<(), std::io::Error> {
     // extra byte that easily detect when the buffer is too small
     let mut recv_buf = [0; BUFFER_SIZE + 1];
     let mut send_buf = Vec::new();
     let my_options = DefaultOptions::new();
+    let mut last_activity = None;
     // infinite loop
     loop {
-        let (size, peer) = socket.recv_from(&mut recv_buf).await?;
-        if size == recv_buf.len() {
-            warn!("Buffer too small for message, discarded");
-            continue;
-        }
-        debug!("got {} bytes from {peer}", size);
-        let mut segments = Vec::new();
-        let mut updates = Vec::new();
-        let mut deserializer = Deserializer::from_slice(&recv_buf[..size], my_options);
-        // read messages in buffer
-        loop {
-            let res = Message::deserialize(&mut deserializer);
-            if let Err(kind) = res.as_ref() {
-                if let bincode::ErrorKind::Io(err) = kind.as_ref() {
-                    if err.kind() == std::io::ErrorKind::UnexpectedEof {
-                        break;
+        if let Ok((size, peer)) = socket.try_recv_from(&mut recv_buf) {
+            last_activity = Some(Instant::now());
+            if size == recv_buf.len() {
+                warn!("Buffer too small for message, discarded");
+                continue;
+            }
+            debug!("got {} bytes from {peer}", size);
+            let mut segments = Vec::new();
+            let mut updates = Vec::new();
+            let mut deserializer = Deserializer::from_slice(&recv_buf[..size], my_options);
+            // read messages in buffer
+            loop {
+                let res = Message::deserialize(&mut deserializer);
+                if let Err(kind) = res.as_ref() {
+                    if let bincode::ErrorKind::Io(err) = kind.as_ref() {
+                        if err.kind() == std::io::ErrorKind::UnexpectedEof {
+                            break;
+                        }
+                    }
+                }
+                let message: Message<K, V> = res.unwrap();
+                match message {
+                    Message::HashSegment(segment) => {
+                        segments.push(segment);
+                    }
+                    Message::Update(update) => {
+                        updates.push(update);
                     }
                 }
             }
-            let message: Message<K, V> = res.unwrap();
-            match message {
-                Message::HashSegment(segment) => {
-                    segments.push(segment);
+            // handle messages
+            if !segments.is_empty() {
+                debug!("got {} segments", segments.len());
+                let mut diffs = Vec::new();
+                let segments = {
+                    let guard = tree.read().unwrap();
+                    guard.diff_round(&mut diffs, segments)
+                };
+                let mut messages = Vec::new();
+                if !segments.is_empty() {
+                    debug!("Split in {} segments", segments.len());
+                    for segment in segments {
+                        messages.push(Message::HashSegment::<K, V>(segment))
+                    }
                 }
-                Message::Update(update) => {
-                    updates.push(update);
+                if !diffs.is_empty() {
+                    let guard = tree.read().unwrap();
+                    info!("Found diffs: {diffs:?}");
+                    for diff in diffs {
+                        for (k, v) in guard.get_range(&diff) {
+                            messages.push(Message::Update((k.clone(), v.clone())));
+                        }
+                    }
+                }
+                if !messages.is_empty() {
+                    debug!("sending {} messages to {peer}", messages.len());
+                    send_buf.clear();
+                    for message in messages {
+                        let last_size = send_buf.len();
+                        message
+                            .serialize(&mut Serializer::new(&mut send_buf, my_options))
+                            .unwrap();
+                        if send_buf.len() > BUFFER_SIZE {
+                            socket.send_to(&send_buf[..last_size], &peer).await?;
+                            debug!("sent {} bytes to {peer}", send_buf.len());
+                            send_buf.drain(..last_size);
+                        }
+                    }
+                    socket.send_to(&send_buf, &peer).await?;
+                    debug!("sent {} bytes to {peer}", send_buf.len());
+                    last_activity = Some(Instant::now());
                 }
             }
+            if !updates.is_empty() {
+                debug!("got {} updates", updates.len());
+                let mut guard = tree.write().unwrap();
+                for (k, v) in updates {
+                    // conflict resolution
+                    // TODO
+                    /*
+                    if let Some(_) = guard.get(&k) {
+                        if true {
+                            guard.insert(k, v);
+                            continue;
+                        }
+                    }
+                    */
+                    guard.insert(k, v);
+                }
+                info!("Updated state; global hash is now {}", guard.hash(&..));
+            }
         }
-        // handle messages
-        if !segments.is_empty() {
-            debug!("got {} segments", segments.len());
-            let mut diffs = Vec::new();
+        let is_active = last_activity
+            .map(|last_activity| Instant::now() - last_activity < Duration::from_secs(1))
+            .unwrap_or(false);
+        if !is_active {
             let segments = {
                 let guard = tree.read().unwrap();
-                guard.diff_round(&mut diffs, segments)
+                guard.start_diff()
             };
-            let mut messages = Vec::new();
-            if !segments.is_empty() {
-                debug!("Split in {} segments", segments.len());
-                for segment in segments {
-                    messages.push(Message::HashSegment::<K, V>(segment))
-                }
+            send_buf.clear();
+            for segment in segments {
+                Message::HashSegment::<K, V>(segment)
+                    .serialize(&mut Serializer::new(&mut send_buf, my_options))
+                    .unwrap();
             }
-            if !diffs.is_empty() {
-                let guard = tree.read().unwrap();
-                info!("Found diffs: {diffs:?}");
-                for diff in diffs {
-                    for (k, v) in guard.get_range(&diff) {
-                        messages.push(Message::Update((k.clone(), v.clone())));
-                    }
-                }
-            }
-            if !messages.is_empty() {
-                debug!("sending {} messages to {peer}", messages.len());
-                send_buf.clear();
-                for message in messages {
-                    let last_size = send_buf.len();
-                    message
-                        .serialize(&mut Serializer::new(&mut send_buf, my_options))
-                        .unwrap();
-                    if send_buf.len() > BUFFER_SIZE {
-                        socket.send_to(&send_buf[..last_size], &peer).await?;
-                        debug!("sent {} bytes to {peer}", send_buf.len());
-                        send_buf.drain(..last_size);
-                    }
-                }
-                socket.send_to(&send_buf, &peer).await?;
-                debug!("sent {} bytes to {peer}", send_buf.len());
-            }
+            debug!("start_diff {} bytes to {other_addr}", send_buf.len());
+            socket.send_to(&send_buf, &other_addr).await?;
+            last_activity = Some(Instant::now());
         }
-        if !updates.is_empty() {
-            debug!("got {} updates", updates.len());
-            let mut guard = tree.write().unwrap();
-            for (k, v) in updates {
-                // conflict resolution
-                // TODO
-                /*
-                if let Some(_) = guard.get(&k) {
-                    if true {
-                        guard.insert(k, v);
-                        continue;
-                    }
-                }
-                */
-                guard.insert(k, v);
-            }
-            info!("Updated state; global hash is now {}", guard.hash(&..));
-        }
-    }
-}
-
-async fn send_queries<K: Clone + Hash + Ord + Serialize, V: Serialize + Hash>(
-    socket: Arc<UdpSocket>,
-    other_addr: SocketAddr,
-    tree: Arc<RwLock<HTree<K, V>>>,
-) -> Result<(), std::io::Error> {
-    let my_options = DefaultOptions::new();
-    let mut send_buf = Vec::new();
-    loop {
-        let segments = {
-            let guard = tree.read().unwrap();
-            guard.start_diff()
-        };
-        send_buf.clear();
-        for segment in segments {
-            Message::HashSegment::<K, V>(segment)
-                .serialize(&mut Serializer::new(&mut send_buf, my_options))
-                .unwrap();
-        }
-        debug!("start_diff {} bytes to {other_addr}", send_buf.len());
-        socket.send_to(&send_buf, &other_addr).await?;
-        tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
 
@@ -190,10 +188,7 @@ async fn main() {
     info!("Global hash is {}", tree.hash(&..));
     let state = Arc::new(RwLock::new(tree));
 
-    let handle_recv = tokio::spawn(answer_queries(Arc::clone(&socket), Arc::clone(&state)));
-    let handle_send = tokio::spawn(send_queries(socket, other_addr, state));
-    match select(handle_recv, handle_send).await {
-        Either::Left((left, _right)) => left.unwrap().unwrap(),
-        Either::Right((right, _left)) => right.unwrap().unwrap(),
-    }
+    answer_queries(Arc::clone(&socket), other_addr, Arc::clone(&state))
+        .await
+        .unwrap();
 }
