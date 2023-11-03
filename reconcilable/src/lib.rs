e27199f18177c232e10161170e73268edfe86e76
Quentin Santos <qsantos@qsantos.fr>
2023-11-03 12:49:40 +0100

    Merge lib.rs and rhtree.rs of reconcilable/

--- reconcilable/src/lib.rs
+++ reconcilable/src/lib.rs
@@ -1,6 +1,9 @@
-pub mod rhtree;
+use core::hash::Hash;
 
-use diff::{DiffRanges, Diffable};
+use chrono::{DateTime, Utc};
+
+use diff::{DiffRanges, Diffable, HashRangeQueryable};
+use htree::HTree;
 
 pub trait Reconcilable: Diffable {
     type Value;
@@ -10,4 +13,51 @@ pub trait Reconcilable: Diffable {
         &self,
         diff_ranges: DiffRanges<Self::Key>,
     ) -> Vec<(Self::Key, Self::Value)>;
+}
+
+type TV<V> = (DateTime<Utc>, V);
+
+impl<K, V> Reconcilable for HTree<K, TV<V>>
+where
+    K: Clone + Hash + Ord,
+    V: Clone + Hash,
+{
+    type Value = TV<V>;
+
+    fn reconcile(&mut self, updates: Vec<(Self::Key, Self::Value)>) -> Option<u64> {
+        let mut updated = false;
+        // here, using `Option::map` is clearer than using `if let Some(…) =` because of the
+        // long match expression
+        #[allow(clippy::option_map_unit_fn)]
+        for (k, tv) in updates {
+            match self.get(&k) {
+                Some(local_tv) => {
+                    if tv.0 > local_tv.0 {
+                        Some(tv)
+                    } else {
+                        None
+                    }
+                }
+                None => Some(tv),
+            }
+            .map(|v| {
+                self.insert(k, v);
+                updated = true;
+            });
+        }
+        updated.then(|| self.hash(&..))
+    }
+
+    fn enumerate_diff_ranges(
+        &self,
+        diff_ranges: diff::DiffRanges<Self::Key>,
+    ) -> Vec<(Self::Key, Self::Value)> {
+        let mut ret = Vec::new();
+        for diff in diff_ranges {
+            for (k, v) in self.get_range(&diff) {
+                ret.push((k.clone(), v.clone()));
+            }
+        }
+        ret
+    }
 }
