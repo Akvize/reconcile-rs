3d38eff382e536af76e14b5abfb020f2bcbfcdac
Quentin Santos <qsantos@qsantos.fr>
2023-11-07 16:12:23 +0100

    Extract message handling in separate function

--- reconcile-service/src/lib.rs
+++ reconcile-service/src/lib.rs
@@ -96,103 +96,121 @@ impl<
             if let Ok(Ok((size, peer))) =
                 timeout(recv_timeout, socket.recv_from(&mut recv_buf)).await
             {
-                if size == recv_buf.len() {
-                    warn!("Buffer too small for message, discarded");
-                    continue;
-                }
-                trace!("received {} bytes from {peer}", size);
-                let mut segments = Vec::new();
-                let mut updates = Vec::new();
-                let mut deserializer = Deserializer::from_slice(&recv_buf[..size], my_options);
-                // read messages in buffer
-                loop {
-                    let res = Message::deserialize(&mut deserializer);
-                    if let Err(kind) = res.as_ref() {
-                        if let bincode::ErrorKind::Io(err) = kind.as_ref() {
-                            if err.kind() == std::io::ErrorKind::UnexpectedEof {
-                                break;
-                            }
-                        }
-                    }
-                    let message: Message<K, V> = res.unwrap();
-                    match message {
-                        Message::HashSegment(segment) => {
-                            segments.push(segment);
-                        }
-                        Message::Update(update) => {
-                            updates.push(update);
-                        }
-                    }
-                }
-                // handle messages
-                if !segments.is_empty() {
-                    debug!("received {} segments", segments.len());
-                    let mut diff_ranges = Vec::new();
-                    let mut out_segments = Vec::new();
-                    {
-                        let guard = self.map.read().unwrap();
-                        guard.diff_round(segments, &mut out_segments, &mut diff_ranges);
-                    }
-                    let mut messages = Vec::new();
-                    if !out_segments.is_empty() {
-                        debug!("returning {} segments", out_segments.len());
-                        trace!("segments: {out_segments:?}");
-                        for segment in out_segments {
-                            messages.push(Message::HashSegment::<K, V>(segment))
-                        }
-                    }
-                    if !diff_ranges.is_empty() {
-                        debug!("returning {} diff_ranges", diff_ranges.len());
-                        trace!("diff_ranges: {diff_ranges:?}");
-                        let guard = self.map.read().unwrap();
-                        for update in guard.enumerate_diff_ranges(diff_ranges) {
-                            messages.push(Message::Update(update));
-                        }
-                    }
-                    if !messages.is_empty() {
-                        debug!("sending {} messages to {peer}", messages.len());
-                        send_buf.clear();
-                        for message in messages {
-                            let last_size = send_buf.len();
-                            message
-                                .serialize(&mut Serializer::new(&mut send_buf, my_options))
-                                .unwrap();
-                            if send_buf.len() > BUFFER_SIZE {
-                                trace!("sending {} bytes to {peer}", last_size);
-                                socket.send_to(&send_buf[..last_size], &peer).await.unwrap();
-                                trace!("sent {} bytes to {peer}", last_size);
-                                send_buf.drain(..last_size);
-                            }
-                        }
-                        trace!("sending last {} bytes to {peer}", send_buf.len());
-                        socket.send_to(&send_buf, &peer).await.unwrap();
-                        trace!("sent last {} bytes to {peer}", send_buf.len());
-                    }
-                }
-                if !updates.is_empty() {
-                    debug!("received {} updates", updates.len());
-                    let mut changed = false;
-                    {
-                        let mut guard = self.map.write().unwrap();
-                        for (k, v) in updates {
-                            let local_v = guard.get(&k);
-                            let do_change = local_v
-                                .map(|local_v| {
-                                    local_v.reconcile(&v) == ReconciliationResult::KeepOther
-                                })
-                                .unwrap_or(true);
-                            if do_change {
-                                before_insert(&k, &v, local_v);
-                                guard.insert(k, v);
-                                changed = true;
-                            }
-                        }
-                    }
-                    if changed {
-                        after_sync(&self);
-                    }
-                }
+                self.handle_messages(
+                    &socket,
+                    &recv_buf,
+                    (size, peer),
+                    &mut send_buf,
+                    &before_insert,
+                    &after_sync,
+                )
+                .await;
                 last_activity = Some(Instant::now());
+            }
+        }
+    }
+
+    async fn handle_messages<FI: Fn(&K, &V, Option<&V>), FU: Fn(&Self)>(
+        &self,
+        socket: &UdpSocket,
+        recv_buf: &[u8],
+        (size, peer): (usize, SocketAddr),
+        send_buf: &mut Vec<u8>,
+        before_insert: &FI,
+        after_sync: &FU,
+    ) {
+        if size == recv_buf.len() {
+            warn!("Buffer too small for message, discarded");
+            return;
+        }
+        trace!("received {} bytes from {peer}", size);
+        let mut segments = Vec::new();
+        let mut updates = Vec::new();
+        let mut deserializer = Deserializer::from_slice(&recv_buf[..size], DefaultOptions::new());
+        // read messages in buffer
+        loop {
+            let res = Message::deserialize(&mut deserializer);
+            if let Err(kind) = res.as_ref() {
+                if let bincode::ErrorKind::Io(err) = kind.as_ref() {
+                    if err.kind() == std::io::ErrorKind::UnexpectedEof {
+                        break;
+                    }
+                }
+            }
+            let message: Message<K, V> = res.unwrap();
+            match message {
+                Message::HashSegment(segment) => {
+                    segments.push(segment);
+                }
+                Message::Update(update) => {
+                    updates.push(update);
+                }
+            }
+        }
+        // handle messages
+        if !segments.is_empty() {
+            debug!("received {} segments", segments.len());
+            let mut diff_ranges = Vec::new();
+            let mut out_segments = Vec::new();
+            {
+                let guard = self.map.read().unwrap();
+                guard.diff_round(segments, &mut out_segments, &mut diff_ranges);
+            }
+            let mut messages = Vec::new();
+            if !out_segments.is_empty() {
+                debug!("returning {} segments", out_segments.len());
+                trace!("segments: {out_segments:?}");
+                for segment in out_segments {
+                    messages.push(Message::HashSegment::<K, V>(segment))
+                }
+            }
+            if !diff_ranges.is_empty() {
+                debug!("returning {} diff_ranges", diff_ranges.len());
+                trace!("diff_ranges: {diff_ranges:?}");
+                let guard = self.map.read().unwrap();
+                for update in guard.enumerate_diff_ranges(diff_ranges) {
+                    messages.push(Message::Update(update));
+                }
+            }
+            if !messages.is_empty() {
+                debug!("sending {} messages to {peer}", messages.len());
+                send_buf.clear();
+                for message in messages {
+                    let last_size = send_buf.len();
+                    message
+                        .serialize(&mut Serializer::new(&mut *send_buf, DefaultOptions::new()))
+                        .unwrap();
+                    if send_buf.len() > BUFFER_SIZE {
+                        trace!("sending {} bytes to {peer}", last_size);
+                        socket.send_to(&send_buf[..last_size], &peer).await.unwrap();
+                        trace!("sent {} bytes to {peer}", last_size);
+                        send_buf.drain(..last_size);
+                    }
+                }
+                trace!("sending last {} bytes to {peer}", send_buf.len());
+                socket.send_to(send_buf, &peer).await.unwrap();
+                trace!("sent last {} bytes to {peer}", send_buf.len());
+            }
+        }
+        if !updates.is_empty() {
+            debug!("received {} updates", updates.len());
+            let mut changed = false;
+            {
+                let mut guard = self.map.write().unwrap();
+                for (k, v) in updates {
+                    let local_v = guard.get(&k);
+                    let do_change = local_v
+                        .map(|local_v| local_v.reconcile(&v) == ReconciliationResult::KeepOther)
+                        .unwrap_or(true);
+                    if do_change {
+                        before_insert(&k, &v, local_v);
+                        guard.insert(k, v);
+                        changed = true;
+                    }
+                }
+            }
+            if changed {
+                after_sync(self);
             }
         }
     }
