21611d43275ddd073629bb07b79e683ba8aed188
Adrien <adriendellagaspera@outlook.com>
2023-11-13 13:23:50 +0100

    Add missing copyrights

--- src/gen_ip.rs
+++ src/gen_ip.rs
@@ -1,3 +1,13 @@
+// Copyright 2023 Developers of the reconcile project.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Provides utility methods to generate IP addresses from a CIDR.
+
 use ipnet::{IpBitAnd, IpBitOr, IpNet, Ipv4Net, Ipv6Net};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
