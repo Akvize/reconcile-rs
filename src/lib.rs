856af794331b8f43332da5ff02b50752e6b0f9de
Adrien <adriendellagaspera@outlook.com>
2023-11-13 10:00:31 +0100

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@ const BUFFER_SIZE: usize = 65507;
 /// To do so, the service also wraps a collection of addresses,
 /// representing the other instances.
 ///
-/// Provides wrappers for its underlying [`Map`](map::Map)'s insertion and deletion methods,
+/// Provides wrappers for its underlying [`Map`]'s insertion and deletion methods,
 /// as well as its main service method: run.
 ///
 /// This struct does not handle removals. See [`RemoveService`].
